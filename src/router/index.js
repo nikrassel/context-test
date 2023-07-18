53934d2b801d99923b53c5267f94a1b9665db80d
nikrassel <nikrassel@mail.ru>
2023-07-18 13:59:53 +0300

    project init

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+  },
+  {
+    path: "/about",
+    name: "about",
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
